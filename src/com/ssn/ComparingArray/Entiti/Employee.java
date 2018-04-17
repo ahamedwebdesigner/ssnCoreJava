6f8e17ca028563e3f1c4c2df0e0232c5e2871791
ahamesye <ahamesye@AHAMESYE1.auth.hpicorp.net>
2018-04-17 19:43:11 +0530

    end of the class

---
+++ src/com/ssn/ComparingArray/Entiti/Employee.java
@@ -0,0 +1,65 @@
+package com.ssn.ComparingArray.Entiti;
+
+public class Employee {
+
+	    private Integer id;
+	    private String firstname;
+	    private String lastName;
+	    private String department;
+	    
+	    
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getFirstname() {
+			return firstname;
+		}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getDepartment() {
+			return department;
+		}
+		public void setDepartment(String department) {
+			this.department = department;
+		}
+	    
+	    
+		@Override
+		public int hashCode() {
+		final int prime = 31;
+			int result = 1;
+			
+			result = prime * result + (
+					                    (id == null) ? 0 
+					                    		     : id.hashCode()
+					                    		);
+		
+			return result;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			
+			
+		
+			Employee e = (Employee) obj;
+		    return (this.getId() == e.getId());
+		}
+
+}
---
+++ src/com/ssn/ComparingArray/Entiti/EqualsTest.java
@@ -0,0 +1,30 @@
+package com.ssn.ComparingArray.Entiti;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EqualsTest {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+        Employee e2 = new Employee();
+ 
+        e1.setId(100);
+        e2.setId(100);
+        
+        System.out.println("========================");
+        System.out.println(e1.hashCode());
+        System.out.println(e2.hashCode());
+        System.out.println("========================");
+        System.out.println(e1.equals(e2));
+        
+        
+        Set<Employee> employees = new HashSet<Employee>();
+        employees.add(e1);
+        employees.add(e2);
+        
+        System.out.println(employees);
+
+	}
+
+}
